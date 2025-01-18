2c1103957cba462754252c4cabf20e27774837f0
Kyle Boas <68482183+kyleboas@users.noreply.github.com>
2025-01-18 11:34:16 -0500

    .

--- js/if-inbound.js
+++ js/if-inbound.js
@@ -1,4 +1,4 @@
-const PROXY_URL = 'https://infiniteflightapi.deno.dev';
+const PROXY_URL = 'https://infiniteflightapi.deno.dev/';
 const SESSION_ID = '9bdfef34-f03b-4413-b8fa-c29949bb18f8';
 
 let allFlights = [];
