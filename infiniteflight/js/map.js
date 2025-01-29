e47bd691ab31b537258160da1847aad0ea72b654
Kyle Boas <68482183+kyleboas@users.noreply.github.com>
2025-01-29 16:29:06 -0500

    .

--- infiniteflight/js/map.js
+++ infiniteflight/js/map.js
@@ -1,4 +1,4 @@
-import { getFlights } from "./inbounds.js"; // Import flight data
+import { getFlights } from "./inbounds-test.js"; // Import flight data
 
 export function showMap(flight) {
     const mapContainer = document.getElementById("mapContainer");
