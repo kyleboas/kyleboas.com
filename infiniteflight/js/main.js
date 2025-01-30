30701a440765aa02e6fc8bb9885a4e6caf202d61
Kyle Boas <68482183+kyleboas@users.noreply.github.com>
2025-01-30 11:40:24 -0500

    .

--- infiniteflight/js/main.js
+++ infiniteflight/js/main.js
@@ -1,6 +1,8 @@
 import { showMap } from "./map.js";
 import { AutoUpdate } from "./AutoUpdate.js";
 import { 
+        getFlights,
+        allFlights,
         fetchAndUpdateFlights,
         interpolateNextPositions,
         fetchControllers,
@@ -29,7 +31,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     // Fetch flight data on load
     try {
-        flights = await getFlights();  // Ensure getFlights() is defined or imported
+        flights = await getFlights());  // Ensure getFlights() is defined or imported
         console.log("Flights loaded:", flights);
     } catch (error) {
         console.error("Error fetching flights:", error);
