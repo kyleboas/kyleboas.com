66d2e775c3ecf7abe75bc12a0e8336a01325ebad
Kyle Boas <68482183+kyleboas@users.noreply.github.com>
2025-02-02 08:57:41 -0500

    .

--- infiniteflight/js/flights.js
+++ infiniteflight/js/flights.js
@@ -2,7 +2,7 @@ import { fetchStatusData } from './api.js';
 import { SESSION_ID } from './config.js';
 
 export async function fetchFlights(icao) {
-    const flights = await fetchStatusData(${icao});
+    const flights = await fetchStatusData(`${icao}`);
     state.allFlights = flights.result || [];
 }
 
