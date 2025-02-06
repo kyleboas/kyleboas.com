431f019d4217c9dc2ca01d7750e6eb0ceb51816d
Kyle Boas <68482183+kyleboas@users.noreply.github.com>
2025-02-06 08:39:43 -0500

    fix?

--- infiniteflight/js/icao.js
+++ infiniteflight/js/icao.js
@@ -1,12 +1,9 @@
-// Store ICAO in sessionStorage
 export function setICAO(icao) {
     console.log("Storing ICAO:", icao);
+    localStorage.setItem("icao", icao);
     sessionStorage.setItem("icao", icao);
 }
 
-// Retrieve ICAO from sessionStorage
 export function getICAO() {
-    const icao = sessionStorage.getItem("icao") || null;
-    console.log("Retrieving ICAO:", icao);
-    return icao;
+    return sessionStorage.getItem("icao") || localStorage.getItem("icao") || null;
 }
