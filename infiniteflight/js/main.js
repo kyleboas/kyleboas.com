a370f1d877e9bd9f6d6c7092327ba7fbce303f3e
Kyle Boas <68482183+kyleboas@users.noreply.github.com>
2025-02-05 21:26:06 -0500

    ..

--- infiniteflight/js/main.js
+++ infiniteflight/js/main.js
@@ -1,4 +1,5 @@
 import { getICAO } from "./icao.js";
 import { inputSearch } from "./search.js";
+import { getFlights, allFlights } from "./inbounds.js";
 
 document.getElementById("icao").addEventListener("input", inputSearch);
