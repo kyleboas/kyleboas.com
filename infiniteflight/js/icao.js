519213e366c6a1c86389a534c64c98c046f09bbe
Kyle Boas <68482183+kyleboas@users.noreply.github.com>
2025-02-06 08:38:28 -0500

    .

--- infiniteflight/js/icao.js
+++ infiniteflight/js/icao.js
@@ -5,7 +5,8 @@ export function setICAO(icao) {
 }
 
 // Retrieve ICAO from sessionStorage
-export function setICAO(icao) {
-    console.log("Storing ICAO:", icao);
-    sessionStorage.setItem("icao", icao);
+export function getICAO() {
+    const icao = sessionStorage.getItem("icao") || null;
+    console.log("Retrieving ICAO:", icao);
+    return icao;
 }
